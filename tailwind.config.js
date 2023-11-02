f68dfe4bbadd0a10b8f40211982cf32c4fa68d4c
deranderemark <mark.steffes04@gmail.com>
2023-11-02 16:26:56 +0100

    .

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./src/**/*.{html,ts}",
+    './components/**/*.{html,js}',
+  ],
   theme: {
+    colors: {
+      'text': 'var(--text)',
+      'background': 'var(--background)',
+      'primary': 'var(--primary)',
+      'secondary': 'var(--secondary)',
+      'accent': 'var(--accent)',
+     },
+     
     extend: {},
   },
   plugins: [],
 }
-
