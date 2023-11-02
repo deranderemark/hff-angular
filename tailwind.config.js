0b33a5dfab9686f037ad00a8cc0c53e877737f9e
deranderemark <mark.steffes04@gmail.com>
2023-11-02 19:50:57 +0100

    fixed tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,9 +5,15 @@ module.exports = {
     './components/**/*.{html,js}',
   ],
   theme: {
-    colors: {},
-     
-    extend: {},
+    extend: {
+      colors: {
+        'text': '#0d120e',
+        'background': '#ccffd6',
+        'primary': '#06fe51',
+        'secondary': '#e8e8e3',
+        'accent': '#178538',
+      },
+    },
   },
   plugins: [],
 }
